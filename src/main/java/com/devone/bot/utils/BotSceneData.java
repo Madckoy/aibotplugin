4c7a8c4d675a55dbbda22d7971c45bf739caa01c
Madckoy <madckoy@gmail.com>
2025-04-11 10:55:50 +0200

    refactor: Rename botLocation to bot for clarity in BotSceneData

--- src/main/java/com/devone/bot/utils/BotSceneData.java
+++ src/main/java/com/devone/bot/utils/BotSceneData.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class BotSceneData {
     public List<BotBlockData> blocks;
     public List<BotBlockData> entities;
-    public BotCoordinate3D botLocation;
+    public BotCoordinate3D bot;
 
     public BotSceneData() {}
 
-    public BotSceneData(List<BotBlockData> blocks, List<BotBlockData> entities, BotCoordinate3D botLocation) {
+    public BotSceneData(List<BotBlockData> blocks, List<BotBlockData> entities, BotCoordinate3D botPos) {
         this.blocks = blocks;
         this.entities = entities;
-        this.botLocation = botLocation;
+        this.bot = botPos;
     }
 }
