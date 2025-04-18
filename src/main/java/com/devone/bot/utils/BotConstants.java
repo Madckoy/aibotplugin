8a199dda6f5448200f204323c56bac0ec96e05e4
Madckoy <madckoy@gmail.com>
2025-04-18 02:26:33 +0200

    fix(BotConstants): update DEFAULT_SCAN_RANGE to 12

--- src/main/java/com/devone/bot/utils/BotConstants.java
+++ src/main/java/com/devone/bot/utils/BotConstants.java
@@ -13,7 +13,7 @@ public class BotConstants {
     public static final String  RESOURCE_PATH_PATTERNS_BREAK = "patterns/break/";
 
 
-    public static final int     DEFAULT_SCAN_RANGE = 8;
+    public static final int     DEFAULT_SCAN_RANGE = 12;
     
     public static final long    DEFAULT_TASK_TIMEOUT = 60_000; // 60 sec
     public static final double  DEFAULT_HAND_DAMAGE = 5.0;
