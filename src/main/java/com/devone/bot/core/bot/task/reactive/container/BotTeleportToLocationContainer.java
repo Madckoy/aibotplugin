da7d69db47f894beaa710405ba5e2e3f065f286a
Madckoy <madckoy@gmail.com>
2025-04-20 20:26:33 +0200

    refactor: update generic type for BotTeleportToLocationContainer to use BotTeleportToPlayerContainerParams

--- src/main/java/com/devone/bot/core/bot/task/reactive/container/BotTeleportToLocationContainer.java
+++ src/main/java/com/devone/bot/core/bot/task/reactive/container/BotTeleportToLocationContainer.java
@@ -4,15 +4,16 @@ import com.devone.bot.core.bot.Bot;
 import com.devone.bot.core.bot.task.active.teleport.BotTeleportTask;
 import com.devone.bot.core.bot.task.active.teleport.params.BotTeleportTaskParams;
 import com.devone.bot.core.bot.task.passive.BotReactiveTaskContainer;
-import com.devone.bot.core.bot.task.passive.params.BotTaskParams;
+
+import com.devone.bot.core.bot.task.reactive.container.params.BotTeleportToPlayerContainerParams;
 import com.devone.bot.core.utils.blocks.BotLocation;
 
-public class BotTeleportToLocationContainer extends BotReactiveTaskContainer<BotTaskParams> {
+public class BotTeleportToLocationContainer extends BotReactiveTaskContainer<BotTeleportToPlayerContainerParams> {
 
     private final BotLocation location;
 
     public BotTeleportToLocationContainer(Bot bot, BotLocation location) {
-        super(bot, BotTaskParams.class);
+        super(bot, BotTeleportToPlayerContainerParams.class);
         this.location = location;
         setIcon("📍");
         setObjective("Контейнер для BotTeleportTask");
