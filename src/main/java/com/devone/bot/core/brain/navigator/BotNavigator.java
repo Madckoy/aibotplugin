31a94cb6b27dfd0e2242e97decfbec191ae1b930
Madckoy <s.kolesnikow@gmail.com>
2025-05-27 15:29:28 +0200

    refactor: Comment out block filtering logic in BotNavigator to retain previously visited blocks

--- src/main/java/com/devone/bot/core/brain/navigator/BotNavigator.java
+++ src/main/java/com/devone/bot/core/brain/navigator/BotNavigator.java
@@ -259,9 +259,9 @@ public class BotNavigator {
             List<BotBlockData> walkableValid = BotTagUtils.getTaggedBlocks(bot.getBrain().getSceneData().blocks, "walkable:*, navigation:valid");
 
             // 🧹 Убираем ранее посещённые блоки
-             walkableValid = reachableValid.stream()
-            .filter(b -> !BotMemoryV2Utils.isBlockVisited(bot, b))
-            .toList();
+            //walkableValid = reachableValid.stream()
+            //.filter(b -> !BotMemoryV2Utils.isBlockVisited(bot, b))
+            //.toList();
 
             if( !walkableValid.isEmpty() ) {
                 candidates = walkableValid;
